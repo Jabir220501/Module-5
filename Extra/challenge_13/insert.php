5ce1c0cb25c4434116bdf7340b8228573a46c24a
Jabir <220501@student.glu.nl>
2023-01-29 15:18:28 +0100

    week 3

---
+++ Extra/challenge_13/insert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./index.php" method="post">
+        <input type="text" name="merk" id="merk" placeholder="merk van de auto"/>
+        <input type="number" name="prijs" id="prijs" placeholder="prijs van de auto"/>
+        <input type="number" name="bouwjaar" id="bouwjaar" placeholder="bouwjaar van de auto"/>
+        <input type="text" name="kenteken" id="kenteken" placeholder="kenteken van de auto"/>
+        <input type="text" name="kleur" id="kleur" placeholder="kleur van de auto"/>
+        <input type="submit">
+    </form>
+</body>
+</html>
--- Week 3/challenge_11/login.php
+++ Week 3/challenge_11/login.php
@@ -1,37 +1,43 @@
+<?php
+session_start();
+
+if (isset($_SESSION["ingelogd"])) {
+  if ($_SESSION["ingelogd"] == true) {
+    header("location: ./home.php");
+    exit();
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/login.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Webshop</title>
 </head>
+
 <body>
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Tabs Titles -->
-    <h2 class="active"> Sign In </h2>
-    <h2 class="inactive underlineHover">Sign Up </h2>
 
-    <!-- Icon -->
-    <!-- <div class="fadeIn first">
-      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
-    </div> -->
-
-    <!-- Login Form -->
-    <form action="" method="POST">
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Login">
-      <input type="password" id="password" class="fadeIn third" name="login" placeholder="Password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+  <main>
+<br>
+    <form method="POST" action="./assets/server/login.php">
+      <input type="text" placeholder="email" name="email" required>
+      <br><br>
+      <input type="password" placeholder="password" name="password" required>
+      <br><br>
+      <input type="submit" name="submit" value="Confirm">
     </form>
-
-    <!-- Remind Passowrd -->
-    <div id="formFooter">
-      <a class="underlineHover" href="#">Forgot Password?</a>
-    </div>
-
-  </div>
-</div>
+    <?php
+    if (isset($_GET["msg"])) {
+    ?>
+      <p style="color: red"><?php echo $_GET["msg"] ?></p>
+    <?php
+    }
+    ?>
+  </main>
 </body>
+
 </html>
